f1e7d6b215cab4925d757c81936891d2349ef85b
Jgarcia <josegarcia19809@gmail.com>
2023-12-20 09:47:27 -0600

    Piratas - Ladrón del tesoro

--- Learning Swift 2024/01_fundamentos/LadronDelTesoro.swift
+++ Learning Swift 2024/01_fundamentos/LadronDelTesoro.swift
@@ -9,9 +9,14 @@
  
  El ladrón del tesoro
 
- Una barco pirata llamado “The hurricane” convivían el 30 tripulantes. Un día el capitán ordenó ir en busca de “El tesoro de las 100 monedas de oro” que se encuentra enterrado en una isla. Cuando llegaron a la isla, del barco desembarcaron 10 piratas para desenterrar el tesoro y llevarlo ante el capitán. Nada más llegar el tesoro al barco el capitán lo abrió y empezó a contar las monedas de oro, percatándose así de que tan sólo había 90 monedas. La conclusión del capitán es que uno de esos 10 piratas que habían desembarcado había robado esas 10 monedas de oro que faltaban.
+ Una barco pirata llamado "The hurricane" convivían el 30 tripulantes. Un día el capitán ordenó ir en busca de “El tesoro de las 100 monedas de oro” que se encuentra enterrado en una isla. Cuando llegaron a la isla, del barco desembarcaron 10 piratas para desenterrar el tesoro y llevarlo ante el capitán. Nada más llegar el tesoro al barco el capitán lo abrió y empezó a contar las monedas de oro, percatándose así de que tan sólo había 90 monedas. La conclusión del capitán es que uno de esos 10 piratas que habían desembarcado había robado esas 10 monedas de oro que faltaban.
 
  ¿El máximo de personas que tendrá que registrar el capitán para saber quién es el culpable, en swift que se definiría como constante (let) o variable (var)?
  */
 
 import Foundation
+
+func PiratasMaximoPersonas(){
+    let maximoPersonas: Int = 30
+    print("Máximo de personas a registrar: \(maximoPersonas)")
+}
