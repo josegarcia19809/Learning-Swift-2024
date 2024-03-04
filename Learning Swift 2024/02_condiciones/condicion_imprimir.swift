8ca57a46f487dbdcdc9fc5264de1f32509e23284
josegarcia19809 <josegarcia19809@gmail.com>
2024-03-03 21:03:24 -0600

    Condición longitud > ancho

---
+++ Learning Swift 2024/02_condiciones/condicion_imprimir.swift
@@ -0,0 +1,16 @@
+//
+//  condicion_imprimir.swift
+//  Learning Swift 2024
+//
+//  Created by Jose Garcia on 03/03/24.
+//
+
+import Foundation
+
+func condicion(){
+    let longitud: Double = 8.0
+    let ancho: Double = 5.0
+    
+    print("¿longitud > ancho?: \(longitud > ancho)")
+    print("¿longitud < ancho?: \(longitud < ancho)")
+}
