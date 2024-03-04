38110b3c9fbd46722e0175bfe587bf387bb842db
josegarcia19809 <josegarcia19809@gmail.com>
2024-03-03 21:40:43 -0600

    Caracteres y concatenación de Strings

--- Learning Swift 2024/09_cadenas/ManipulacionStrings.swift
+++ Learning Swift 2024/09_cadenas/ManipulacionStrings.swift
@@ -6,3 +6,37 @@
 //
 
 import Foundation
+
+func manipulacionStrings(){
+    imprimirCaracteres()
+    declararCaracter()
+    usarArrayCaracteresFormarCadena()
+    agregarAlFinal()
+}
+
+func imprimirCaracteres(){
+    for letra in "Perrito 🐶"{
+        print(letra , terminator: " " )
+    } // P e r r i t o   🐶
+    print()
+}
+
+func declararCaracter(){
+    let questionMark: Character = "?"
+    print(questionMark)
+}
+
+func usarArrayCaracteresFormarCadena(){
+    let dogArray: [Character] = ["p", "e", "r", "r", "o", "🐶"]
+    let dogString: String = String(dogArray)
+    print(dogString)
+}
+
+func agregarAlFinal(){
+    var mensajeCompleto: String = "Bienvenida"
+    print("Agrega un nombre: ")
+    let nombre: String = readLine() ?? ""
+    mensajeCompleto+=" "
+    mensajeCompleto.append(nombre)
+    print(mensajeCompleto)
+}
