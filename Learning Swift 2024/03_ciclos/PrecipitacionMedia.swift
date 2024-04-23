5623492763b66f44551c0e1a4a929ee40e70e4b3
josegarcia19809 <josegarcia19809@gmail.com>
2024-04-22 21:18:55 -0600

    Precipitación media. FOR

--- Learning Swift 2024/03_ciclos/PrecipitacionMedia.swift
+++ Learning Swift 2024/03_ciclos/PrecipitacionMedia.swift
@@ -6,3 +6,36 @@
 //
 
 import Foundation
+
+func precipitacionMedia(){
+    var anios: Int
+    let meses: [String] = ["Enero", "Febrero", "Marzo"]
+    var totalLluvia: Int = 0
+    var totalMeses: Int = 0
+    
+    print("Escribe el número de años de medición: ")
+    anios = Int(readLine() ?? "0") ?? 0
+    
+    // Validar que años sea mayor que cero
+    if anios < 1{
+        print("Número de años no válido")
+        return
+    }
+    
+    for anio in 1...anios{
+        for mes in meses{
+            print("Ingresa las pulgadas de lluvia para el mes de \(mes) del año \(anio):")
+            let lluvia: Int
+            lluvia = Int(readLine() ?? "0") ?? 0
+            totalLluvia += lluvia
+            totalMeses += 1
+        }
+    }
+    
+    let promedio_lluvia = Double(totalLluvia) / Double(totalMeses)
+    
+    // Mostrar resultados
+    print("Número total de meses: \(totalMeses)")
+    print("Total de pulgadas de lluvia: \(totalLluvia)")
+    print("Precipitación promedio: \(promedio_lluvia)")
+}
--- Learning Swift 2024/main.swift
+++ Learning Swift 2024/main.swift
@@ -8,4 +8,4 @@
 import Foundation
 
 print("Programando en Swift...")
-tabuladorSalarios()
+precipitacionMedia()
