596204c419d814d375bac4464c7c0fd80918c396
josegarcia19809 <josegarcia19809@gmail.com>
2024-05-23 19:35:23 -0600

    Sucesión Aritmética 1

--- Learning Swift 2024/03_ciclos/sucesion_aritmetica_1.swift
+++ Learning Swift 2024/03_ciclos/sucesion_aritmetica_1.swift
@@ -6,3 +6,15 @@
 //
 
 import Foundation
+
+func sucesionAritmetica1() {
+    var importeInicial: Double = 800.00
+    var totalAPagar: Double = importeInicial
+    for noArticulo in 1 ... 49 {
+        var importeElemento = importeInicial + (300.00 * Double(noArticulo))
+        totalAPagar += importeElemento
+        print("\(noArticulo + 1): \(importeElemento). Pagando \(totalAPagar)")
+    }
+    print("-----------------------")
+    print("Total a pagar: $\(String(format: "%.2f", totalAPagar))")
+}
