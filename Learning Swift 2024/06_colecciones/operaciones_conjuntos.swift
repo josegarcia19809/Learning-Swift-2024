d6a4760dc8b2d0001cd3c5357de414b61ce23bd6
josegarcia19809 <josegarcia19809@gmail.com>
2024-06-13 21:33:55 -0600

    Operaciones con conjuntos

--- Learning Swift 2024/06_colecciones/operaciones_conjuntos.swift
+++ Learning Swift 2024/06_colecciones/operaciones_conjuntos.swift
@@ -6,3 +6,27 @@
 //
 
 import Foundation
+
+func operacionesConjuntos () {
+    print("Operaciones con conjuntos")
+    let pares : Set = [0,2,4,6,8]
+    let impares : Set = [1,3,5,7,9]
+    let primos : Set = [2,3,5,7]
+    
+    print("Unión pares e impares")
+    print(pares.union(impares).sorted())
+    print()
+    
+    print("Intersección pares e impares")
+    print(pares.intersection(impares).sorted())
+    print()
+    
+    print("Sustracción impares con primos")
+    print(impares.subtracting(primos).sorted())
+    print()
+    
+    print("Diferencia simétrica pares y primos")
+    print(pares.symmetricDifference(primos).sorted())
+    print()
+    imprimirLinea()
+}
---
+++ Learning Swift 2024/06_colecciones/subconjuntos.swift
@@ -0,0 +1,8 @@
+//
+//  subconjuntos.swift
+//  Learning Swift 2024
+//
+//  Created by Jose Garcia on 13/06/24.
+//
+
+import Foundation
